50ef22344b2848b44e198b96ab397b3508ea3900
David Ramalho <dramalho@gmail.com>
2017-08-07 18:04:42 +0100

    Complete all optional functions

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -1,32 +1,33 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
-%   taking num_iters gradient steps with learning rate alpha
+  %GRADIENTDESCENT Performs gradient descent to learn theta
+  %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by
+  %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
+  % Initialize some useful values
+  m = length(y); % number of training examples
+  J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
+  for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta.
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-    predictions   = X * theta       % m x 1 vector
-    error_vector  = predictions - y % m x 1 vector
-    product       = X' * error_vector
+      % ====================== YOUR CODE HERE ======================
+      % Instructions: Perform a single gradient step on the parameter vector
+      %               theta.
+      %
+      % Hint: While debugging, it can be useful to print out the values
+      %       of the cost function (computeCost) and gradient here.
+      %
+      predictions   = X * theta          % m x 1 vector
+      error_vector  = predictions - y    % m x 1 vector
+      product       = X' * error_vector  % [n x m] X [m x 1]
 
-    theta = theta - alpha * 1/m * product
+      theta = theta - alpha * 1/m * product % scaling of the product
 
 
-    % ============================================================
+      % ============================================================
 
-    % Save the cost J in every iteration
-    J_history(iter) = computeCost(X, y, theta);
-end
+      % Save the cost J in every iteration
+      J_history(iter) = computeCost(X, y, theta);
+  end
 
+  plot(J_history)
 end
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,25 +11,21 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
+    predictions   = X * theta          % m x 1 vector
+    error_vector  = predictions - y    % m x 1 vector
+    product       = X' * error_vector  % [n x m] X [m x 1]
 
-
-
-
-
-
-
-
-
+    theta = theta - alpha * 1/m * product % scaling of the product
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
