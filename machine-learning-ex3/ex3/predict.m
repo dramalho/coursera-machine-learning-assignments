cde3b0437d5e3bce992b67c56cce0982c9f1b2d4
David Ramalho <dramalho@gmail.com>
2017-10-15 13:11:57 +0100

    Complete week 4 exercise

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -20,6 +20,26 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+
+% % once I figure out Arrays of variables in octave VS vectors :) I'll clean this up
+% thetas = [Theta1;Theta2]
+% previous_layer = X
+%
+% for layer = 1:size(thetas, 2)
+%   m = size(previous_layer, 1)
+%
+%   previous_layer = sigmoid([ones(m, 1) previous_layer] * thetas(layer)' )
+% endfor
+
+
+layer_1 = sigmoid([ones(m, 1) X] * Theta1')
+
+m = size(layer_1, 1)
+layer_2 = sigmoid([ones(m, 1) layer_1] * Theta2')
+
+[max_value, p] = max(previous_layer, [], 2)
+
+
 
 
 
